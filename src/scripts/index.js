36e95a1f0c886c0d68b9cb99765627125b42f151
yoseffourentino <yoseffourentino30@gmail.com>
2024-05-10 00:51:49 +0700

    still progres

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,22 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import data from '../public/data/data.js'
 
-console.log('Hello Coders! :)');
+const contentContainer = document.getElementById('content-container');
+
+data.forEach((e) => {
+    console.log(e);
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <div>
+            <h3 class="resto-name">${e.name}</h3>
+            <img src="${e.pictureId}" alt="">
+            <h5 class="rating"><i class="fa-solid fa-star"></i> ${e.rating}</h5>
+            <p class="desc">${e.description}</p>
+            <h4 class="city"><i class="fa-solid fa-location-dot"></i> ${e.city}</h4>
+        </div>
+    `
+    contentContainer.append(card);
+});
+
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { watch } = require('fs');
 
 module.exports = {
   entry: {
@@ -27,12 +28,13 @@ module.exports = {
       },
     ],
   },
+  watch: true,
   plugins: [
     new CleanWebpackPlugin(),
 
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
     }),
     new CopyWebpackPlugin({
       patterns: [
